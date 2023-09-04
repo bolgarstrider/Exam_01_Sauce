33c3f437fd713863d42c254fdd0355fe2abc1a82
bolgarstrider <george.gergov@gmail.com>
2023-09-04 22:09:54 +0300

    Added explicit wait in LoginTest

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,6 +1,8 @@
 package tests;
 import base.TestUtil;
 import com.opencsv.exceptions.CsvException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -8,6 +10,10 @@ import pages.LoginPage;
 import pages.ProductsPage;
 import utils.CSVHelper;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.NoSuchElementException;
+
 public class LoginTests extends TestUtil {
 
 
@@ -27,13 +33,24 @@ public class LoginTests extends TestUtil {
     public void successfulLogin(String username, String password){
         LoginPage loginPage = new LoginPage(driver);
         ProductsPage productsPage = loginPage.login(username, password);
-        Assert.assertTrue(driver.getCurrentUrl().equals("https://www.saucedemo.com/inventory.html"));
+
+        String currentUrl = driver.getCurrentUrl();
+
+        FluentWait fluentWait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(5))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoreAll(Collections.singleton(NoSuchElementException.class));
+
+        fluentWait.until(ExpectedConditions.urlToBe(currentUrl));
+
+        Assert.assertTrue(currentUrl.equals("https://www.saucedemo.com/inventory.html"));
     }
 
     @Test(dataProvider =  "csvWrongCredentials")
     public void unsuccessfulLogin(String username, String password){
         LoginPage loginPage = new LoginPage(driver);
         loginPage.tryToLogin(username, password);
+
         Assert.assertTrue(loginPage.isErrorMsgDisplayed());
     }
 
